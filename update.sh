a3d43acf6919609c213f03c5213468f5d3cc418a
rewasu91 <95635242+rewasu91@users.noreply.github.com>
2021-12-22 16:50:48 +0800

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#text,A=Merah,B=Hijau,C=Magenta,D=Cyan
+A='\033[1;31m'
+B='\033[1;32m'
+C='\033[1;35m'
+D='\033[1;36m'
+#background,A1=Merah,B1=Hijau,C1=Magenta,D1=Cyan
+A1='\033[1;41m'
+B1='\033[1;42m'
+C1='\033[1;45m'
+D1='\033[1;46m'
+#reset
+R='\033[0m'
+
+clear
+echo -e ""
+echo -e ""
+cowsay -f ghostbusters "SELAMAT DATANG BOSKU."
+echo -e ""
+figlet -k '         KAIZEN'
+echo -e ""
+echo -e "${D}——————————————————————————————————————————————————————————${R}"
+echo -e "${D1}                   Menu Update Skrip                      ${R}"
+echo -e "${D}——————————————————————————————————————————————————————————${R}"
+echo -e ""
+echo -e "► Sila tunggu, sistem sedang disemak.."
+sleep 5
+
+clear
+echo -e ""
+echo -e ""
+cowsay -f ghostbusters "SELAMAT DATANG BOSKU."
+echo -e ""
+figlet -k '         KAIZEN'
+echo -e ""
+echo -e "${D}——————————————————————————————————————————————————————————${R}"
+echo -e "${D1}                   Menu Update Skrip                      ${R}"
+echo -e "${D}——————————————————————————————————————————————————————————${R}"
+echo -e ""
+echo -e "► Tiada sebarang update buat masa ini.."
+echo -e ""

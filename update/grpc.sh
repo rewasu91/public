82fee63069a7b399b189ef74cd4adf4cb3197aad
Kaizen <95635242+rewasu91@users.noreply.github.com>
2022-06-18 00:26:48 +0800

    Create grpc.sh

---
+++ update/grpc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /usr/bin
+
+port-xraygrp
+
+wget -O port-xraygrpc "https://raw.githubusercontent.com/rewasu91/server/main/editport/port-xraygrpc.sh"
+
+chmod +x port-xraygrpc
+
+cd
+
+clear

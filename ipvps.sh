56febfa25ac55ed7bac924dfd1c3d711b250f69b
Kaizen <95635242+rewasu91@users.noreply.github.com>
2022-06-30 22:56:19 +0800

    Create ipvps.sh

---
+++ ipvps.sh
@@ -0,0 +1,6 @@
+### Kaizen Lifetime 51.79.243.61
+### Kaizen Lifetime 51.79.240.202
+### Kaizen Lifetime 51.79.242.211
+### Kaizen Lifetime 51.79.240.21
+### Kaizen Lifetime 51.79.243.80
+### Kaizen Lifetime 51.79.254.107
